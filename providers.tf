baf3f7583f3728760ee903248a8b9c7ad555c9f7
Aaron Evans <aarone@outlook.com.au>
2023-11-28 11:51:21 +1100

    updated

--- providers.tf
+++ providers.tf
@@ -10,11 +10,11 @@ terraform {
 
 provider "dns" {
   update {
-    server = var.dns_server
+    server = var.dns_server # Using the hostname is important in order for an SPN to match
     gssapi {
       realm    = var.dns_realm
       username = var.dns_username
       password = var.dns_password
     }
-  }
+  }       
 }
