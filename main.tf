afda6a47e10111b8c2db56fb8692493477208e28
Aaron Evans <aarone@outlook.com.au>
2023-11-28 22:35:20 +1100

    updates

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ resource "dns_a_record_set" "a_record" {
   zone      = "hashicorp.local."
 }
 
-resource "dns_ptr_record" "ptr_record" {
+/*resource "dns_ptr_record" "ptr_record" {
   for_each = { for r in var.a_records : r.name => r }
 
   zone = format("%s.in-addr.arpa.",
                 join(".", slice(reverse(split(".", each.value.addresses[0])), 0, 3)))
   name = element(reverse(split(".", each.value.addresses[0])), 0)
   ptr  = format("%s.%s", each.value.name, "hashicorp.local.")
-}
+}*/
 
 
 resource "dns_cname_record" "cname_record" {
