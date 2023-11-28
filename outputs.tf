97d73fc44dc2c2273ac4eb112579fc01ecf810f3
Aaron Evans <aarone@outlook.com.au>
2023-11-28 11:51:39 +1100

    updated

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "a_record_ids" {
-  value = [for record in dns_a_record_set.a_record : record.id]
+  value = { for k, v in dns_a_record_set.a_record : k => v.id }
 }
 
-output "ptr_record_ids" {
-  value = [for record in dns_ptr_record.ptr_record : record.id]
-}
+/*output "ptr_record_ids" {
+  value = { for k, v in dns_ptr_record.ptr_record : k => v.id }
+}*/
 
 output "cname_record_ids" {
-  value = [for record in dns_cname_record.cname_record : record.id]
+  value = { for k, v in dns_cname_record.cname_record : k => v.id }
 }
