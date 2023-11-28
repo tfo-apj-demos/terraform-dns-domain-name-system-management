75285f80e55ba278cb8d9384898abb273305dc21
Aaron Evans <aarone@outlook.com.au>
2023-11-28 14:54:33 +1100

    updated

--- example/main.tf
+++ example/main.tf
@@ -1,13 +1,11 @@
 module "domain-name-system-management" {
   source  = "app.terraform.io/tfo-apj-demos/domain-name-system-management/dns"
   version = "1.0.4"
-
-  dns_server = "dc-0.hashicorp.local"
   
   a_records = [
     {
       name      = "test"
-      addresses = ["172.21.15.160"]
+      addresses = [ "172.21.15.160" ]
     }
   ]
 }
