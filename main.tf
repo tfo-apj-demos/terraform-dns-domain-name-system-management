4cd0a12f51910f81adc64b9dfbc069ce09bd337b
Aaron Evans <aarone@outlook.com.au>
2023-11-28 22:26:14 +1100

    update for ptr records

--- main.tf
+++ main.tf
@@ -8,13 +8,12 @@ resource "dns_a_record_set" "a_record" {
 resource "dns_ptr_record" "ptr_record" {
   for_each = { for r in var.a_records : r.name => r }
 
-  zone = format("%s.%s.%s.in-addr.arpa.",
-    element(reverse(split(".", each.value.addresses[0])), 0),
-    element(reverse(split(".", each.value.addresses[0])), 1),
-    element(reverse(split(".", each.value.addresses[0])), 2))
+  zone = format("%s.in-addr.arpa.",
+                join(".", slice(reverse(split(".", each.value.addresses[0])), 0, 3)))
   name = element(reverse(split(".", each.value.addresses[0])), 0)
   ptr  = format("%s.%s", each.value.name, "hashicorp.local.")
 }
+
 
 resource "dns_cname_record" "cname_record" {
   for_each = { for r in var.cname_records : r.name => r }
