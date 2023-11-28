de54f69ceff65e4dd07d2eaa2f8978c0f781ba21
Aaron Evans <aarone@outlook.com.au>
2023-11-28 22:02:22 +1100

    clean up example

--- example/main.tf
+++ example/main.tf
@@ -16,17 +16,18 @@ variable "dns_server" {
 
 module "domain-name-system-management" {
   source  = "app.terraform.io/tfo-apj-demos/domain-name-system-management/dns"
-  #source = "../../terraform-dns-management"
   version = "~> 1.0"
 
   dns_server   = var.dns_server
   dns_realm    = var.dns_realm
   dns_password = var.dns_password
   dns_username = var.dns_username
+
   a_records = [
     {
       name      = "test"
       addresses = [ "172.21.15.160" ]
     }
   ]
+
 }
