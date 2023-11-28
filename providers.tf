c99d67b60d1fb5912cf96944d93ca7c02ea5cade
Aaron Evans <aarone@outlook.com.au>
2023-11-28 14:24:04 +1100

    updated

--- providers.tf
+++ providers.tf
@@ -12,9 +12,6 @@ provider "dns" {
   update {
     server = var.dns_server # Using the hostname is important in order for an SPN to match
     gssapi {
-      realm    = var.dns_realm
-      username = var.dns_username
-      password = var.dns_password
     }
   }       
 }
