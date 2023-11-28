9aa462c03c14b6468f5fe0f0d2546e79e662e60e
Aaron Evans <aarone@outlook.com.au>
2023-11-28 11:51:06 +1100

    updated

--- main.tf
+++ main.tf
@@ -1,23 +1,27 @@
 resource "dns_a_record_set" "a_record" {
-  count     = length([for record in var.dns_records : record if record.record_type == "A"])
-  zone      = var.dns_records[count.index].zone
-  name      = var.dns_records[count.index].name
-  addresses = var.dns_records[count.index].addresses
-  ttl       = var.dns_records[count.index].ttl
+  for_each  = { for r in var.a_records : r.name => r }
+  name      = each.value.name
+  addresses = each.value.addresses
+  zone      = "hashicorp.local."
+  ttl       = 604800
 }
 
-resource "dns_ptr_record" "ptr_record" {
-  count = length([for record in var.dns_records : record if record.record_type == "PTR"])
-  zone  = var.dns_records[count.index].zone
-  name  = var.dns_records[count.index].name
-  ptr   = var.dns_records[count.index].addresses[0] // Assuming single address for PTR
-  ttl   = var.dns_records[count.index].ttl
-}
+/*resource "dns_ptr_record" "ptr_record" {
+  for_each = { for r in var.a_records : r.name => r }
+
+  zone = format("%s.%s.%s.in-addr.arpa.",
+    element(reverse(split(".", each.value.addresses[0])), 0),
+    element(reverse(split(".", each.value.addresses[0])), 1),
+  element(reverse(split(".", each.value.addresses[0])), 2))
+  name = element(reverse(split(".", each.value.addresses[0])), 0)
+  ptr  = format("%s.%s", each.value.name, "hashicorp.local.")
+  ttl  = 604800
+}*/
 
 resource "dns_cname_record" "cname_record" {
-  count = length([for record in var.dns_records : record if record.record_type == "CNAME"])
-  zone  = var.dns_records[count.index].zone
-  name  = var.dns_records[count.index].name
-  cname = var.dns_records[count.index].cname
-  ttl   = var.dns_records[count.index].ttl
+  for_each = { for r in var.cname_records : r.name => r }
+  name     = each.value.name
+  cname    = each.value.cname
+  zone     = "hashicorp.local."
+  ttl      = 604800
 }
