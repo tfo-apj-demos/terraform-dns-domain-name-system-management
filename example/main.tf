4da1e4e7f40440362a543a8847fa05e424fe5642
Aaron Evans <aarone@outlook.com.au>
2023-11-28 21:04:56 +1100

    test

--- example/main.tf
+++ example/main.tf
@@ -1,17 +1,41 @@
-variable "dns_server" {
-  description = "DNS Server FQDN"
-  type = string
+terraform {
+  required_providers {
+    dns = {
+      source  = "hashicorp/dns"
+      version = "~> 3.3"
+    }
+  }
+}
+provider "dns" {
+  update {
+    server = var.dns_server # Using the hostname is important in order for an SPN to match
+  }
 }
 
-module "domain-name-system-management" {
-  source  = "app.terraform.io/tfo-apj-demos/domain-name-system-management/dns"
-  version = "1.0.4"
+variable "dns_server" {
+  description = "DNS Server FQDN"
+  type        = string
+}
+
+resource "dns_a_record_set" "this" {
+  name = "test"
+  addresses = [
+    "172.21.15.160"
+  ]
+  zone = "hashicorp.local."
+}
+
+
+/*module "domain-name-system-management" {
+  #source  = "app.terraform.io/tfo-apj-demos/domain-name-system-management/dns"
+  source = "../../terraform-dns-management"
+  #version = "1.0.4"
   
-  dns_server = var.dns_server
+  dns_server = "dc-0.hashicorp.local"
   a_records = [
     {
       name      = "test"
       addresses = [ "172.21.15.160" ]
     }
   ]
-}
+}*/
